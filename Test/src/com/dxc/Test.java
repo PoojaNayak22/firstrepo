240c25182622d95d16c4d50910abdf1c0970bb85
PoojaNayak22 <poojanayakrsrs22@gmail.com>
2020-09-21 20:46:27 +0530

    java code

---
+++ Test/src/com/dxc/Test.java
@@ -0,0 +1,11 @@
+package com.dxc;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hey Pooja..!!Welcome to Git CLI "); 
+	}
+
+}
